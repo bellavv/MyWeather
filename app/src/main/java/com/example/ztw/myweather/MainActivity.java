3de0624eec6c19d2fca0e76e2ca3673cf1f2d2f3
PKUer_ztw <625581835@qq.com>
2018-10-16 21:11:12 +0800

    66666

--- app/src/main/java/com/example/ztw/myweather/MainActivity.java
+++ app/src/main/java/com/example/ztw/myweather/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ztw.myweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Message;
@@ -27,6 +28,7 @@ import org.xmlpull.v1.XmlPullParserFactory;
  */
 public class MainActivity extends ActionBarActivity implements View.OnClickListener{
     private ImageView mUpdateBtn;
+    private ImageView mCitySelect;
 
     private static final int UPDATE_TODAY_WEATHER = 1;
     private TextView cityTv, timeTv, humidityTv, weekTv, pmDataTv,pmQualityTv,temperatureTv, climateTv, windTv, city_name_Tv;
@@ -61,6 +63,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
             Log.d("my Weather", "网络挂了" );
             Toast.makeText(MainActivity.this,"网络挂了！", Toast.LENGTH_LONG).show();
         }
+            mCitySelect = (ImageView) findViewById(R.id.title_city_manager);
+            mCitySelect.setOnClickListener(this);
         initView();
     }
 
@@ -87,10 +91,18 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         temperatureTv.setText("N/A");
         climateTv.setText("N/A");
         windTv.setText("N/A");
+        //updateTodayWeather(TodayWeather todayWeather);
     }
 
     @Override
     public void onClick(View view) {
+        if(view.getId() == R.id.title_city_manager)
+        {
+            Intent i = new Intent(this,SelectCity.class);
+            //startActivity(i);
+            startActivityForResult(i,1);
+
+        }
         if (view.getId() == R.id.title_update_btn){
             SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
             String cityCode = sharedPreferences.getString("main_city_code","101010100");
@@ -106,6 +118,24 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
             }
         }
     }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == 1 && resultCode == RESULT_OK)
+        {
+            String newCityCode= data.getStringExtra("cityCode");
+            Log.d("myWeather", "选择的城市代码为"+newCityCode);
+            if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE)
+            {
+                Log.d("myWeather", "网络OK");
+                queryWeatherCode(newCityCode);
+            }
+            else
+            {
+                Log.d("myWeather", "网络挂了");
+                Toast.makeText(MainActivity.this, "网络挂了！", Toast.LENGTH_LONG).show();
+            }
+    }    }
+
     /**
      *
      * @param cityCode
