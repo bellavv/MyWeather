a87273b5a8a6c13d09f13a2434ed6e4b76264208
PKUer_ztw <625581835@qq.com>
2018-10-22 22:55:50 +0800

    '88888'

--- app/src/main/java/com/example/ztw/bean/City.java
+++ app/src/main/java/com/example/ztw/bean/City.java
@@ -9,7 +9,32 @@ public class City {
     private String number;
     private String firstPY;
     private String allPY;
-    private String allFristPY;
+
+    public void setAllFirstPY(String allFirstPY) {
+        this.allFirstPY = allFirstPY;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setFirstPY(String firstPY) {
+        this.firstPY = firstPY;
+    }
+
+    public void setAllPY(String allPY) {
+        this.allPY = allPY;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    private String allFirstPY;
 
     public String getProvince() {
         return province;
@@ -31,16 +56,16 @@ public class City {
         return allPY;
     }
 
-    public String getAllFristPY() {
-        return allFristPY;
+    public String getAllFirstPY() {
+        return allFirstPY;
     }
-    public City(String province, String city, String number, String firstPY, String allPY, String allFristPY) {
+    public City(String province, String city, String number, String firstPY, String allPY, String allFirstPY) {
         this.province = province;
         this.city = city;
         this.number = number;
         this.firstPY = firstPY;
         this.allPY = allPY;
-        this.allFristPY = allFristPY;
+        this.allFirstPY = allFirstPY;
     }
 
 
